2ec32e1c3718941426f58ba1beb6f9235b400dff
Vitaliy Matveev <vmatveev@itgroup.lc>
2015-09-26 11:31:09 +0300

    Переработана анимация

--- application.js
+++ application.js
@@ -1,93 +1,227 @@
 $(function(){
 	console.log("start");
-	// var paper = Raphael("lines");
-	 //paper.setViewBox(0,0,1680,1050);
-	 //var path = paper.path("m .44703,371.50424 0,622.88135").attr({"stroke":"#3eeec1","stroke-width":"50"}); 
-	  //var transformedPath = paper.path("m 792.45387,375.85333 12.64442,0 57.7549,569.01929 -63.07643,0 z").attr({"fill":"000"}).glow({fill:true,he});
-	  	//animate({"fill":"90-#000-#fff"},1000);
-	 // path.animate({path:transformedPath},1000);
-	 //paper.rect(961.57306, 251.37711, 67.29351, 750.79449).attr({"fill":"#3eeec1","stroke":"none"});
-	// paper.path("m 4.97459,1040.594 29.30565,-68.47519 14.86725,10e-6 30.16338,68.47518").attr("id","test");
-	// paper.path("m 23.80829,1015.159 35.48049,0");
-	// paper.path("M 12.50385,1040.5193 39.06884,979.47269").attr({"stroke":"#3fb5dd"});
-	// paper.path("m 39.2118,978.97235 5.0034,0.0715 25.87474,61.89925");
-	// paper.path("m 41.827085,991.54507 6.59631,17.06093 -13.76176,0.022 z");
-	// paper.path("m 20.55625,1041.1575 8.1484,-18.1552 26.16065,0.072 8.00545,17.9408");
-	// paper.setFinish().attr({"stroke-width":"3"}).transform("t0,-952.36204")	
-	
+	var i=0,
+		m = 50;
 
-	addAnimate('a1',25,35);
-	addAnimate("a2",27,31);
-	addAnimate("a3",31,40);
-	addAnimate("a4",33,36);
-	addAnimate("a5",33,44);
-	addAnimate("a6",35,46);
 
-	addAnimate('v1',29,35);
-	addAnimate("v2",32,39);
-	addAnimate("v3",37,49);
-	addAnimate("v4",38,44);
-	
-	addAnimate('e1',35,45);
-	addAnimate("e2",36,43);
-	addAnimate("e3",39,49);
-	addAnimate("e4",40,46);
-	addAnimate("e5",43,52);
+	// addAnimate("lit4",29,39,{"scaleX":1});
+function setVelocity(){
+if(i<=24){
+	var diff = (1000-400)/12;
+	var val = $("#gradient_position").data("h");
+	if(val==null){
+		val = 400;
+	}
 
-	addAnimate('n1',41,51);
-	addAnimate("n2",43,48);
-	addAnimate("n3",47,54);
-	addAnimate("n4",48,50);
-	addAnimate("n5",50,57);
-
-	addAnimate('u1',44,55);
-	addAnimate("u2",47,52);
-	addAnimate("u3",50,58);
-	addAnimate("u4",51,61);
-	
-	addAnimate('e21',48,60);
-	addAnimate("e22",51,56);
-	addAnimate("e23",53,63);
-	addAnimate("e24",54,66);
-	addAnimate("e25",56,62);
-
-	addAnimate("lit1",13,20);
-	addAnimate("lit2",16,22);
-	addAnimate("lit3",18,30);
-	addAnimate("lit5",24,35);
-	$("#gradient_position").velocity({ y1: 1005, y2: 1000},{
-		"duration":12*50, 
-		"complete":function(){
-			$("#gradient_position").attr({y1:400,y2:401});
-			$("#gradient stop:first-child").attr({"stop-opacity":1});
-			$("#gradient stop:last-child").attr({"stop-opacity":0});
-			$("#gradient_position").velocity({ y1: 1000, y2: 1005},12*50);			
-			
+	val+=diff;
+		$("#gradient_position").data("h",val);
+	console.log("velocity",val,diff);
+	if(i<12){
+		if(val>=950)
+			val=950;
+	$("#gradient_position").attr({ y1: val+5, y2: val});
+	}else{
+			// if(i==13){
+			// 	$("#gradient_position").data("h",400);
+			// 	$("#gradient_position").attr({y1:400,y2:401});
+			//  		$("#gradient stop:first-child").attr({"stop-opacity":1});
+			//  	$("#gradient stop:last-child").attr({"stop-opacity":0});
+			// }
+			$("#gradient_position").velocity({ y1: val, y2: val+5},50);
 		}
-	});	
-	
-	addAnimate("lit1",13,20);
-	addAnimate("lit2",16,22);
-	addAnimate("lit3",18,30);
-	addAnimate("lit5",24,35);
-	$("#litera").css({"transform": "skew(-25deg)"});
+	}
+}
 
+	//prepare
+	$("#litera").css({"transform": "skew(-25deg)"});
 	$("#litera").css({"-moz-transform" : "skew(-25deg) translateX(500px)"});
-	$("#wrapper").delay(21*50).velocity({rotateZ:"-25deg"},(70-21)*50);
-	$("#lit4").velocity({scaleX:"0"},0).delay(29*50).velocity({scaleX:"1"},(39-29)*50);
+	$("#lit4").css("transform", "scaleX(0deg)");
+
+
 
 	$("body").on("mousewheel", function(e) {
+		console.log(i);
+		clearInterval(chrome);
+		step(i++);
+		// run(i++);
+		// run(i++);
 		// console.log(e.deltaX, e.deltaY, e.deltaFactor);
 		// $("#linearGradient12756").velocity({ y1: 1000, y2: 1100},1000)
-		
+		$
 	})
-	function addAnimate(el, from, to){		
-		var m = 50
-		del = from*m;
-		dur = (to-from)*m;		
-		$("#"+el).delay(del).velocity({"stroke-dashoffset":0},{
-			duration:dur,
-			"easing": "easeInSine"
-		})
+
+	// var js = "switch(i){";
+ //  	function addTimer(js, obj, start, delay){
+ //  		js+= "case"+ start+": addAnimate('" + obj+"',"+delay+");break;"
+ //  	}
+ //  	addTimer(js, "lit1", 13, 7);
+ //  	addTimer(js, "lit2", 16, 6);
+ //  	addTimer(js, "lit3", 18, 12);
+ //  	addTimer(js, "lit4", 24, 35);
+ //  	js+="}"
+ //  	eval(js);
+ 	function step(i){
+		setVelocity();
+
+ 		registerStroke("#lit1", 13, 33, 3000);
+  	registerStroke("#lit2", 16, 38, 3000);
+  	registerStroke("#lit3", 18, 48, 3000);
+	  registerStroke("#lit5", 24, 59, 3000);
+
+	  registerStroke("#a1", 25, 35, 200);
+	  registerStroke("#a2", 27, 31, 200);
+	  registerStroke("#a3", 31, 40, 200);
+	  registerStroke("#a4", 33, 36, 200);
+	  registerStroke("#a5", 33, 44, 200);
+	  registerStroke("#a6", 35, 46, 200);
+
+	  registerStroke("#v1", 29, 35, 200);
+	  registerStroke("#v2", 32, 39, 200);
+	  registerStroke("#v3", 37, 49, 200);
+	  registerStroke("#v4", 38, 44, 200);
+
+	  registerStroke("#e1", 35, 45, 200);
+	  registerStroke("#e2", 36, 43, 200);
+	  registerStroke("#e3", 39, 49, 200);
+	  registerStroke("#e4", 40, 46, 200);
+	  registerStroke("#e5", 43, 52, 200);
+
+	  registerStroke('#n1',41,51, 200);
+		registerStroke("#n2",43,48, 200);
+		registerStroke("#n3",47,54, 200);
+		registerStroke("#n4",48,50, 200);
+		registerStroke("#n5",50,57, 200);
+
+		registerStroke('#u1',44,55, 200);
+		registerStroke("#u2",47,52, 200);
+		registerStroke("#u3",50,58, 200);
+		registerStroke("#u4",51,61, 200);
+
+		registerStroke('#e21',48,60, 200);
+		registerStroke("#e22",51,56, 200);
+		registerStroke("#e23",53,63, 200);
+		registerStroke("#e24",54,66, 200);
+		registerStroke("#e25",56,62, 200);
+		registerRotate("#wrapper", 21, 70);
+
+		registerScale("#lit4",29,39);
+ 	}
+	var chrome = setInterval(function() {
+  	i++;
+  	step(i);
+
+  	console.log(i);
+  	if(i>75)
+  		clearInterval(chrome);
+	}, m)
+	// setTimeout(function(){clearInterval(chrome)},4000);
+
+	// function register(el,from,to,type){
+	// 	if(i>from&&i<to)
+	// 		setAttr(el,"stroke-dashoffset", 3000/(to-from), 0);
+	// }
+	function setAttr(el, param, diff, limit){
+		var val = $(el).data("val");
+		if(val==null){
+			val = parseFloat($(el).css(param));
+		}
+		var source = val;
+		val -= diff;
+		console.log(el,source, val,diff)
+		if(val<limit){
+			val = limit;
+		}
+		$(el).css(param,val);
+		$(el).data("val",val);
 	}
+
+	function setScale(el, param, diff, limit){
+		var val = $(el).data("val");
+		if(val==null){
+			val = 0;
+		}
+		console.log(el, val,diff)
+		val += diff;
+		// console.log(el, val,diff)
+		if(val>limit){
+			val = limit;
+		}
+		$(el).css("transform",param+"("+val+")");
+		$(el).data("val",val);
+	}
+
+	function setTransform(el, param, diff, limit){
+		var style = $(el).attr("style");
+		if(style==null){
+			val = 0;
+		}else{
+		var val = parseFloat($(el).attr("style").split("(")[1])
+		}
+
+		val = (val+diff);
+		val += "deg";
+		$(el).css("transform",param+"("+val+")");
+	}
+
+	function registerStroke(el, from, to, dashoffset){
+		if(i>=from&&i<=to)
+			setAttr(el,"stroke-dashoffset", dashoffset/(to-from), 0);
+	}
+	function registerScale(el, from, to){
+		if(i>from&&i<to)
+			setScale(el,"scaleX", 1/(to-from), 1);
+	}
+	function registerRotate(el, from, to){
+		if(i>from&&i<to)
+			setTransform(el,"rotateZ", -25/(to-from), -25);
+	}
+	// function addAnimate(el, from, to, param, debbug){
+
+	// 	if(i<from){
+	// 		setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, m);
+	// 	} else {
+	// 		if (param == null){
+	// 			param = {"stroke-dashoffset":0};
+	// 		}
+	// 		dur = (to-from)*m;
+	// 		if(debbug===true){
+	// 			if(i>=to)
+	// 				return
+
+	// 		dur = m;
+	// 		var diff = (-25/(70-21));
+	// 		$("#"+el).css("transform","rotateZ("+param["rotateZ"]+")");
+	// 		var val = parseFloat(param["rotateZ"])+diff;
+	// 		param["rotateZ"]= val+"deg";
+	// 		console.log("debbub",param);
+	// 		setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, dur);
+	// 		return;
+	// 		}
+
+	// 		// console.log(param);
+
+	// 		animates.push($("#"+el).velocity(param,{
+	// 			duration:1,
+	// 			 "easing": "linear",
+	// 			complete :function(e){
+	// 				if(debbug===true){
+	// 					var val = parseFloat(param["rotateZ"])+diff;
+	// 					param["rotateZ"]= val+"deg";
+	// 					console.log("debbub",param);
+	// 					setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, dur);
+	// 				}
+	// 			}
+	// 		}));
+
+	// 		}
+	// }
+
+
 })
+
+function move(param){
+	var k = 100;
+	var diff = (-25/k);
+	var val = parseFloat(param["rotateZ"])+diff;
+
+}
