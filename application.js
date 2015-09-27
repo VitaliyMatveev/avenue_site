1fbce82017edf842961a3e106f351ffdcbf375b3
Vitaliy Matveev <vmatveev@itgroup.lc>
2015-09-27 12:03:46 +0300

    Добавлено:  фон и его анимация  откат анимации

--- application.js
+++ application.js
@@ -1,227 +1,212 @@
+var animations = [];
 $(function(){
 	console.log("start");
-	var i=0,
-		m = 50;
+	var i=1,
+		m = 40;
+
 
 
 	// addAnimate("lit4",29,39,{"scaleX":1});
-function setVelocity(){
-if(i<=24){
-	var diff = (1000-400)/12;
-	var val = $("#gradient_position").data("h");
-	if(val==null){
-		val = 400;
-	}
 
-	val+=diff;
-		$("#gradient_position").data("h",val);
-	console.log("velocity",val,diff);
-	if(i<12){
-		if(val>=950)
-			val=950;
-	$("#gradient_position").attr({ y1: val+5, y2: val});
-	}else{
-			// if(i==13){
-			// 	$("#gradient_position").data("h",400);
-			// 	$("#gradient_position").attr({y1:400,y2:401});
-			//  		$("#gradient stop:first-child").attr({"stop-opacity":1});
-			//  	$("#gradient stop:last-child").attr({"stop-opacity":0});
-			// }
-			$("#gradient_position").velocity({ y1: val, y2: val+5},50);
-		}
-	}
-}
 
 	//prepare
 	$("#litera").css({"transform": "skew(-25deg)"});
 	$("#litera").css({"-moz-transform" : "skew(-25deg) translateX(500px)"});
 	$("#lit4").css("transform", "scaleX(0deg)");
+	$("#litera path, #logo path").each(function(i,e){
+		var length = e.getTotalLength();
+	  $(e).css({"stroke-dashoffset": length,
+    					"stroke-dasharray": [length,length]});
+	});
 
 
 
 	$("body").on("mousewheel", function(e) {
-		console.log(i);
+		// console.log(i, e.originalEvent.deltaY);
 		clearInterval(chrome);
+		if(e.originalEvent.deltaY>0){
 		step(i++);
-		// run(i++);
-		// run(i++);
-		// console.log(e.deltaX, e.deltaY, e.deltaFactor);
-		// $("#linearGradient12756").velocity({ y1: 1000, y2: 1100},1000)
-		$
+		animations.forEach(function(a){a.step(i)});
+	}	else{
+			localStorage.setItem("forward", "false");
+			console.log(localStorage)
+			animations.forEach(function(a){a.rollback()})
+			setTimeout(function() {
+				chrome = run();
+				i=0;
+			}, 500);
+
+
+		}
 	})
 
-	// var js = "switch(i){";
- //  	function addTimer(js, obj, start, delay){
- //  		js+= "case"+ start+": addAnimate('" + obj+"',"+delay+");break;"
- //  	}
- //  	addTimer(js, "lit1", 13, 7);
- //  	addTimer(js, "lit2", 16, 6);
- //  	addTimer(js, "lit3", 18, 12);
- //  	addTimer(js, "lit4", 24, 35);
- //  	js+="}"
- //  	eval(js);
+
+		new Animation("#lit1", 13, 33, 0, "stroke-dashoffset");
+  	new Animation("#lit2", 16, 38, 0, "stroke-dashoffset");
+  	new Animation("#lit3", 18, 48, 0, "stroke-dashoffset");
+	  new Animation("#lit5", 24, 59, 0, "stroke-dashoffset");
+
+	  new Animation("#a1", 25, 35, 0, "stroke-dashoffset");
+	  new Animation("#a2", 27, 31, 0, "stroke-dashoffset");
+	  new Animation("#a3", 31, 40, 0, "stroke-dashoffset");
+	  new Animation("#a4", 33, 36, 0, "stroke-dashoffset");
+	  new Animation("#a5", 33, 44, 0, "stroke-dashoffset");
+	  new Animation("#a6", 35, 46, 0, "stroke-dashoffset");
+
+	  new Animation("#v1", 29, 35, 0, "stroke-dashoffset");
+	  new Animation("#v2", 32, 39, 0, "stroke-dashoffset");
+	  new Animation("#v3", 37, 49, 0, "stroke-dashoffset");
+	  new Animation("#v4", 38, 44, 0, "stroke-dashoffset");
+
+	  new Animation("#e1", 35, 45, 0, "stroke-dashoffset");
+	  new Animation("#e2", 36, 43, 0, "stroke-dashoffset");
+	  new Animation("#e3", 39, 49, 0, "stroke-dashoffset");
+	  new Animation("#e4", 40, 46, 0, "stroke-dashoffset");
+	  new Animation("#e5", 43, 52, 0, "stroke-dashoffset");
+
+	  new Animation('#n1',41,51, 0, "stroke-dashoffset");
+		new Animation("#n2",43,48, 0, "stroke-dashoffset");
+		new Animation("#n3",47,54, 0, "stroke-dashoffset");
+		new Animation("#n4",48,50, 0, "stroke-dashoffset");
+		new Animation("#n5",50,57, 0, "stroke-dashoffset");
+
+		new Animation('#u1',44,55, 0, "stroke-dashoffset");
+		new Animation("#u2",47,52, 0, "stroke-dashoffset");
+		new Animation("#u3",50,58, 0, "stroke-dashoffset");
+		new Animation("#u4",51,61, 0, "stroke-dashoffset");
+
+		new Animation('#e21',48,60, 0, "stroke-dashoffset");
+		new Animation("#e22",51,56, 0, "stroke-dashoffset");
+		new Animation("#e23",53,63, 0, "stroke-dashoffset");
+		new Animation("#e24",54,66, 0, "stroke-dashoffset");
+		new Animation("#e25",56,62, 0, "stroke-dashoffset");
+
+		new Animation("#lit4",29,39, 1, "scaleX", 0, -1/(39-29));
+		new Animation("#wrapper", 21, 70, -25, "rotateZ", 0, 25/(70-21))
+		new Animation("#gradient_position",0,12,980,"gradient_1", 400, -(980-400)/(11))
+		new Animation("#gradient_position",13,21,980,"gradient_2", 400, 	-(980-400)/(21-13))
+
+		new Animation(".center-title.top div",42,53, 0, "margin-top", 100, 100/(53-41));
+		new Animation(".center-title.bottom div",45,55, 0, "margin-top", -100, -100/(53-42));
+		new Animation(".background",18,28, 1, "opacity", 0, -1/(24-18));
+		new Animation(".background",18,42, 1, "scale", 1.1, 0.1/(42-18));
+		//transition: opacity 300ms linear, transform 2100ms linear;
  	function step(i){
-		setVelocity();
 
- 		registerStroke("#lit1", 13, 33, 3000);
-  	registerStroke("#lit2", 16, 38, 3000);
-  	registerStroke("#lit3", 18, 48, 3000);
-	  registerStroke("#lit5", 24, 59, 3000);
-
-	  registerStroke("#a1", 25, 35, 200);
-	  registerStroke("#a2", 27, 31, 200);
-	  registerStroke("#a3", 31, 40, 200);
-	  registerStroke("#a4", 33, 36, 200);
-	  registerStroke("#a5", 33, 44, 200);
-	  registerStroke("#a6", 35, 46, 200);
-
-	  registerStroke("#v1", 29, 35, 200);
-	  registerStroke("#v2", 32, 39, 200);
-	  registerStroke("#v3", 37, 49, 200);
-	  registerStroke("#v4", 38, 44, 200);
-
-	  registerStroke("#e1", 35, 45, 200);
-	  registerStroke("#e2", 36, 43, 200);
-	  registerStroke("#e3", 39, 49, 200);
-	  registerStroke("#e4", 40, 46, 200);
-	  registerStroke("#e5", 43, 52, 200);
-
-	  registerStroke('#n1',41,51, 200);
-		registerStroke("#n2",43,48, 200);
-		registerStroke("#n3",47,54, 200);
-		registerStroke("#n4",48,50, 200);
-		registerStroke("#n5",50,57, 200);
-
-		registerStroke('#u1',44,55, 200);
-		registerStroke("#u2",47,52, 200);
-		registerStroke("#u3",50,58, 200);
-		registerStroke("#u4",51,61, 200);
-
-		registerStroke('#e21',48,60, 200);
-		registerStroke("#e22",51,56, 200);
-		registerStroke("#e23",53,63, 200);
-		registerStroke("#e24",54,66, 200);
-		registerStroke("#e25",56,62, 200);
-		registerRotate("#wrapper", 21, 70);
-
-		registerScale("#lit4",29,39);
+		if(i==12){
+			$("#gradient_position").attr({y1:405,y2:400});
+			$("#gradient stop:first-child").attr({"stop-opacity":1});
+		 	$("#gradient stop:last-child").attr({"stop-opacity":0});
+	  }
  	}
-	var chrome = setInterval(function() {
-  	i++;
-  	step(i);
+ 	function run(){
+ 	if(localStorage.getItem("forward")=="true"){
+ 		console.log("forward",localStorage);
+ 		animations.forEach(function(a){a.forward()})
+ 	}else{
+	return setInterval(function() {
+	  	i++;
+	  	step(i);
+	  	animations.forEach(function(a){a.step(i)})
+	  	console.log(i);
 
-  	console.log(i);
-  	if(i>75)
-  		clearInterval(chrome);
-	}, m)
-	// setTimeout(function(){clearInterval(chrome)},4000);
+	  	if(i>75){
+	  		clearInterval(chrome);
+	  	  localStorage.setItem("forward", "true");
+	  	}
+		}, m);
+	}
+}
+	var chrome = run();
 
-	// function register(el,from,to,type){
-	// 	if(i>from&&i<to)
-	// 		setAttr(el,"stroke-dashoffset", 3000/(to-from), 0);
-	// }
-	function setAttr(el, param, diff, limit){
-		var val = $(el).data("val");
-		if(val==null){
-			val = parseFloat($(el).css(param));
+ })
+
+function Animation(el, from, to, limit, param_name, value, diff){
+	this.$el = $(el);
+	this.param_name = param_name;
+	this.from = from;
+	this.to = to;
+	this.limit = limit;
+	if(param_name=="stroke-dashoffset" && value==null){
+		this.val = this.$el[0].getTotalLength();
+	} else {
+		this.val = value;
+	}
+
+	this.default_value = this.val;
+
+	this.diff = diff?diff:this.val/(to-from);
+	animations.push(this);
+}
+
+Animation.prototype.rollback = function(){
+		// console.log("rollback",this.$el.selector,this.default_value);
+		this.$el.css("transition","all 500ms ease-out");
+		this.val = this.default_value;
+		if(this.param_name == "rotateZ"){
+			this.$el.css("transform","rotateZ("+this.default_value+"deg)");
+		} else if(this.param_name == "scaleX"){
+			this.$el.css("transform", "scaleX("+this.default_value+")");
+		} else if(this.param_name == "scale"){
+			this.$el.css("transform", "scale("+this.default_value+")");
+		}else if(this.param_name =="gradient_1"){
+			this.$el.attr({ y1: this.default_value+5, y2: this.default_value});
+		} else if(this.param_name =="gradient_2"){
+			this.$el.attr({ y1: this.default_value, y2: this.default_value+5});
+		}else {
+			this.$el.css(this.param_name,this.default_value);
 		}
-		var source = val;
-		val -= diff;
-		console.log(el,source, val,diff)
-		if(val<limit){
-			val = limit;
+
+
+	}
+
+Animation.prototype.forward = function(){
+	// console.log("forward",this.$el.selector,this.limit);
+	this.$el.css("transition","all 0ms ease-out");
+	this.val = this.limit;
+	if(this.param_name == "rotateZ"){
+		this.$el.css("transform","rotateZ("+this.val+"deg)");
+	} else if(this.param_name == "scaleX"){
+		this.$el.css("transform", "scaleX("+this.val+")");
+	} else if(this.param_name == "scale"){
+			this.$el.css("transform", "scale("+this.default_value+")");
+	// } else if(this.param_name =="gradient_1"){
+	// 	this.$el.attr({ y1: this.val+5, y2: this.val});
+	} else if(this.param_name =="gradient_2"){
+		this.$el.attr({ y1: this.val, y2: this.val+5});
+	}else {
+		this.$el.css(this.param_name,this.val);
+	}
+}
+Animation.prototype.step = function(i){
+	this.$el.css("transition","all 50ms ease");
+	if(this.from<=i && i<=this.to){
+
+		this.val -= this.diff;
+
+
+
+			if($.inArray(this.param_name,["scaleX","gradient_1","gradient_2","opacity"])>-1){
+				if(this.val>this.limit){
+					this.val = this.limit;
+				}
+			}else if(this.param_name=="margin-top"){
+
+			}else{
+				if(this.val<this.limit){
+					this.val = 	this.limit;
+				}
+			}
+		// }
+		// console.log(this.$el.selector,this.diff,this.val,this);
+
+		switch(this.param_name){
+			case "rotateZ": this.$el.css("transform", "rotateZ("+this.val+"deg)"); break;
+			case "scaleX":  this.$el.css("transform", "scaleX("+this.val+")"); break;
+			case "scale":  this.$el.css("transform", "scale("+this.val+")"); break;
+			case "gradient_1": this.$el.attr({ y1: this.val+5, y2: this.val}); break;
+			case "gradient_2": this.$el.attr({ y1: this.val, y2: this.val+5}); break;
+			default: this.$el.css(this.param_name,this.val);
 		}
-		$(el).css(param,val);
-		$(el).data("val",val);
 	}
-
-	function setScale(el, param, diff, limit){
-		var val = $(el).data("val");
-		if(val==null){
-			val = 0;
-		}
-		console.log(el, val,diff)
-		val += diff;
-		// console.log(el, val,diff)
-		if(val>limit){
-			val = limit;
-		}
-		$(el).css("transform",param+"("+val+")");
-		$(el).data("val",val);
-	}
-
-	function setTransform(el, param, diff, limit){
-		var style = $(el).attr("style");
-		if(style==null){
-			val = 0;
-		}else{
-		var val = parseFloat($(el).attr("style").split("(")[1])
-		}
-
-		val = (val+diff);
-		val += "deg";
-		$(el).css("transform",param+"("+val+")");
-	}
-
-	function registerStroke(el, from, to, dashoffset){
-		if(i>=from&&i<=to)
-			setAttr(el,"stroke-dashoffset", dashoffset/(to-from), 0);
-	}
-	function registerScale(el, from, to){
-		if(i>from&&i<to)
-			setScale(el,"scaleX", 1/(to-from), 1);
-	}
-	function registerRotate(el, from, to){
-		if(i>from&&i<to)
-			setTransform(el,"rotateZ", -25/(to-from), -25);
-	}
-	// function addAnimate(el, from, to, param, debbug){
-
-	// 	if(i<from){
-	// 		setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, m);
-	// 	} else {
-	// 		if (param == null){
-	// 			param = {"stroke-dashoffset":0};
-	// 		}
-	// 		dur = (to-from)*m;
-	// 		if(debbug===true){
-	// 			if(i>=to)
-	// 				return
-
-	// 		dur = m;
-	// 		var diff = (-25/(70-21));
-	// 		$("#"+el).css("transform","rotateZ("+param["rotateZ"]+")");
-	// 		var val = parseFloat(param["rotateZ"])+diff;
-	// 		param["rotateZ"]= val+"deg";
-	// 		console.log("debbub",param);
-	// 		setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, dur);
-	// 		return;
-	// 		}
-
-	// 		// console.log(param);
-
-	// 		animates.push($("#"+el).velocity(param,{
-	// 			duration:1,
-	// 			 "easing": "linear",
-	// 			complete :function(e){
-	// 				if(debbug===true){
-	// 					var val = parseFloat(param["rotateZ"])+diff;
-	// 					param["rotateZ"]= val+"deg";
-	// 					console.log("debbub",param);
-	// 					setTimeout(function(){ addAnimate(el, from, to, param, debbug)}, dur);
-	// 				}
-	// 			}
-	// 		}));
-
-	// 		}
-	// }
-
-
-})
-
-function move(param){
-	var k = 100;
-	var diff = (-25/k);
-	var val = parseFloat(param["rotateZ"])+diff;
-
 }
